4a03ea052d5f3b34bebf864da90c304d625ed499
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-03 16:13:23 -0500

    14 passed, 14 total

--- main.js
+++ main.js
@@ -1,11 +1,61 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
+function moreThan5 (num) {
+  return num > 5;
+}
 
+function topScore (topScore, score) {
+  return topScore > score;
+}
 
+function isInDanger (grade) {
+  return grade >= 60 && grade <= 71;
+}
 
+function isCoasting (grade) {
+  return grade >=72 && grade <= 83;
+}
 
+function isSucceeding (grade) {
+  return grade >=84 && grade <=92;
+}
 
+function isFailing (grade) {
+  return grade < 60;
+}
+
+function isAcing (grade) {
+  return grade > 92;
+}
+
+function isStudent (strStudentRole) {
+  return strStudentRole === 'student';
+}
+
+function isTeacher (strTeacherRole) {
+  return strTeacherRole === 'teacher';
+}
+
+function isAdmin (strAdminRole) {
+  return strAdminRole === 'admin';
+}
+
+function isElementary (strElementaryLevel) {
+  return strElementaryLevel === 'elementary';
+}
+
+function isMiddleSchoolTeacher (strMiddleSchoolLevel, numLevel) {
+  return strMiddleSchoolLevel === 'teacher' && numLevel >= 6 && numLevel <= 8;
+}
+
+function notAnElementarySchoolAdministrator (notElementary, notSchoolAdministrator) {
+  return notElementary !== 'elementary' || notSchoolAdministrator !== 'admin';
+}
+
+function differentPeople (name1, name2) {
+  return name1 !== name2;
+}
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
  *                              THIS IS FOR INTERNAL USE ONLY                               *
